175d1dbb5d16391e2e50c97a4be5d4367722db17
Vivek R A <139916865+vivek-37@users.noreply.github.com>
2026-02-26 20:28:22 +0530

    Refactor HTTP response construction in app.cpp

--- backend/app.cpp
+++ backend/app.cpp
@@ -43,10 +43,13 @@ int main() {
         if (client_fd < 0) continue;
         
         // Simple HTTP response
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
+
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
         response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
+        response += body;
         
         send(client_fd, response.c_str(), response.length(), 0);
         close(client_fd);
